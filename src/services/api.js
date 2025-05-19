486b70ef18c66488a4677ad0f844dc1d088a11f0
medud <simooued1@gmail.com>
2025-05-19 12:49:55 +0100

    frontend

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 // Create axios instance with environment variable support
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
+
 const api = axios.create({
   baseURL: API_URL,
   headers: {
